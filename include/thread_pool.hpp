80c87b1ce287670ea321e3936d52a769844b019a
yangdi <di.yang@hongjingdrive.com>
2024-05-23 20:01:04 +0800

    添加池内线程自适应增减的sample

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -45,11 +45,7 @@ class ThreadPool {
   ThreadPool(int maxCount, int coreCount, int tQueueLength, Policy p, int lTime, Unit u, bool is_batch_io);
   ~ThreadPool();
 
-  /**
-   * 形参为右值引用，形参将从实参中“窃取”（移动赋值/移动构造）数据，避免了数据拷贝过程提高了性能。
-   * 但是这个版本的函数只能接受 非const的右值类型的实参。
-   * 当实参是右值时优先匹配这个函数。
-   */
+  // Task 移动构造的版本
   template<class Func, class... Args>
   bool Commit(Func&& task, Args&&... args) {
     bool ret = true;
@@ -57,21 +53,20 @@ class ThreadPool {
 
     if (index < coreThreadCount) {
       // 进核心线程本地队列
-      std::cout << "push to core thread[" << index << "] local task queue\n";
+      std::cout << "Push to core thread[" << index << "] local task queue\n";
       core_thread_queue_[index]->PushTask(std::move(std::bind(task, args...)));
     } else if (pool_task_queue_.Size() < taskQueueLength) {
       // 池任务队列不满，进池任务队列
-      std::cout << "push to pool task queue\n";
+      std::cout << "Push to pool task queue\n";
       pool_task_queue_.Push(std::move(std::bind(task, args...)));
       cv.notify_one();
     } else if (livingThread < maxThreadCount) {
       // 队列满，开非核心线程
-      std::cout<<"new std::thread \n";
+      std::cout<<"Create none core thread\n";
       none_core_thread_queue_.push_back(std::thread(&ThreadPool::none_core_thread_function, this, std::bind(task, args...)));
       livingThread++;
     } else {
       // 队列满，线程数也到达最大线程数，调用拒绝策略
-      // std::cout<<"reject task \n";
       RejectPolicyFactory::getInstance().getRejectPolicy(policy)->reject(std::bind(task,args...));
       ret = false;
     }
@@ -79,55 +74,58 @@ class ThreadPool {
     return ret;
   }
 
-  /**
-   * 形参为const的左值引用，形参将从实参中“拷贝”（拷贝赋值/拷贝构造）数据
-   * 这个版本的函数能接受任意类型实参对象（左右值都可以），但是由于数据需要从实参拷贝到形参，所以性能不如使用右值引用的函数
-   * 当不存在形参为右值引用的函数时，这个const 左值引用为形参的函数也可以使用
-   */
-  // template<class Func, class... Args>
-  // bool Commit(Func const& task, Args const& ...args) {
-  //   bool ret = true;
-  //   if(livingThread >= coreThreadCount && getCurrentTaskQueueSize() < taskQueueLength) {
-  //     taskQueue.push(std::bind(task, args...));//任务队列没满先让任务进队列。
-  //     cv.notify_one();
-  //     runningThread++;
-  //   }
-  //   else if(livingThread < maxThreadCount && getCurrentTaskQueueSize() >= taskQueueLength) {
-  //     //任务队列满了，但是活跃的线程数小于最大线程数，则开新的线程并将任务直接交给新线程。
-  //     noneCoreThreadQueue.push_back(std::thread(&ThreadPool::threadFunction, this, std::bind(task, args...)));
-  //     livingThread++;
-  //   }
-  //   else if(livingThread >= maxThreadCount && getCurrentTaskQueueSize() >= taskQueueLength) {
-  //     //reject，队列满，线程数也到达最大线程数，这个时候调用拒绝策略
-  //     RejectPolicyFactory::getInstance()->getRejectPolicy(policy)->reject(std::bind(task,args...));
-  //     ret = false;
-  //   }
-  //   return ret;
-  // }
+  // Task 拷贝构造的版本
+  template<class Func, class... Args>
+  bool Commit(Func const& task, Args const& ...args) {
+    bool ret = true;
+    int index{get_real_index()};
+
+    if (index < coreThreadCount) {
+      // 进核心线程本地队列
+      std::cout << "Push to core thread[" << index << "] local task queue\n";
+      core_thread_queue_[index]->PushTask(std::move(std::bind(task, args...)));
+    } else if (pool_task_queue_.Size() < taskQueueLength) {
+      // 池任务队列不满，进池任务队列
+      std::cout << "Push to pool task queue\n";
+      pool_task_queue_.Push(std::move(std::bind(task, args...)));
+      cv.notify_one();
+    } else if (livingThread < maxThreadCount) {
+      // 队列满，开非核心线程
+      std::cout<<"Create none core thread \n";
+      none_core_thread_queue_.push_back(std::thread(&ThreadPool::none_core_thread_function, this, std::bind(task, args...)));
+      livingThread++;
+    } else {
+      // 队列满，线程数也到达最大线程数，调用拒绝策略
+      RejectPolicyFactory::getInstance().getRejectPolicy(policy)->reject(std::bind(task,args...));
+      ret = false;
+    }
+
+    return ret;
+  }
 
   int  getRunningThreadCount();
   int  getLivingThreadCount();
   int  getCurrentTaskQueueSize();
 
  private:
-    std::vector<std::shared_ptr<CoreThread>> core_thread_queue_; // 核心本地队列线程
-    std::vector<std::thread>                 none_core_thread_queue_; //非核心线程队列
-    StealQueue<Task>                    pool_task_queue_;
-    std::atomic<int>                    runningThread;
-    std::atomic<int>                    runningTasks;
-    std::atomic<int>                    livingThread;
-    std::mutex                          mutex_;
-    std::condition_variable             cv;
-    int                                 liveTime;
-    Unit                                unit;
-    Policy                              policy;
-    bool                                isShutdown;
-    int                                 maxThreadCount;
-    int                                 coreThreadCount;
-    int                                 taskQueueLength;
-    int                                 task_index_;
-    int                                 none_core_thread_index_;
-    bool                                is_batch_io_;
+    std::vector<std::shared_ptr<CoreThread>>  core_thread_queue_; // 核心本地队列线程
+    std::vector<std::thread>                  none_core_thread_queue_; //非核心线程队列
+    StealQueue<Task>                          pool_task_queue_;
+    std::atomic<int>                          runningThread;
+    std::atomic<int>                          runningTasks;
+    std::atomic<int>                          livingThread;
+    std::mutex                                mutex_;
+    std::condition_variable                   cv;
+    int                                       liveTime;
+    Unit                                      unit;
+    Policy                                    policy;
+    bool                                      isShutdown;
+    int                                       maxThreadCount;
+    int                                       coreThreadCount;
+    int                                       taskQueueLength;
+    int                                       task_index_;
+    int                                       none_core_thread_index_;
+    bool                                      is_batch_io_;
 
     /* Functions  */ 
     void  none_core_thread_function(std::function<void()>&&); //线程函数，起来以后是独立线程，任务函数执行在这个函数中
--- sample/src/sample-thread_adapt.cpp
+++ sample/src/sample-thread_adapt.cpp
@@ -8,15 +8,21 @@ void task(int a) {
 }
 
 void thread_num_adapt_case(ThreadPool& pool) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 100; j++) {
+      pool.Commit(task, j);
+    }
 
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+  }
 }
 
 int main() {
-  int maxCount = 8;
+  int maxCount = 18;
   int coreCount = 8;
-  int taskQueueLength = 0;
+  int taskQueueLength = 20;
   Policy policy = Discard;
-  int liveTime = 2;
+  int liveTime = 1;
   ThreadPool::Unit unit = ThreadPool::Unit::Second;
   ThreadPool pool(maxCount, coreCount, taskQueueLength, policy, liveTime, unit);
 
